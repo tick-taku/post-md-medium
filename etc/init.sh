4ca7f414e02a6f9984b68b8da5ff21add7645058
tick-taku <tick.taku.77@gmail.com>
2021-05-01 17:57:53 +0900

    Install pipenv if not found

--- etc/init.sh
+++ etc/init.sh
@@ -5,3 +5,11 @@ read -p "Integration token: " integration_token
 cp secrets/secrets_sample.py ./secrets.py
 sed -i -e "s/INTEGRATION_TOKEN =/INTEGRATION_TOKEN = \"$integration_token\"/" secrets.py
 rm -rf secrets.py-e
+
+if [ "$(command -v pip)" == '' ]; then
+	curl -kL https://bootstrap.pypa.io/get-pip.py | python
+fi
+
+if [ "$(pip list | grep pipenv)" == '' ]; then
+	pip install pipenv
+fi
