585b323ff91cf431127968cb59b9919a0a4baa26
tick-taku <tick.taku.77@gmail.com>
2021-05-02 16:17:00 +0900

    Setup from python.

--- etc/init.py
+++ etc/init.py
@@ -1,3 +1,15 @@
 import subprocess
 
-subprocess.run('./etc/init.sh')
+integration_token = input('integration token: ')
+
+subprocess.run(['cp', 'secrets/secrets_sample.py', './secrets.py'])
+subprocess.run(['sed', '-i', '-e', f's/INTEGRATION_TOKEN =/INTEGRATION_TOKEN = "{integration_token}"/', 'secrets.py'])
+subprocess.run(['rm', '-rf', 'secrets.py-e'])
+
+pip_list = subprocess.Popen(['pip', 'list'], stdout=subprocess.PIPE)
+grep_requests = subprocess.Popen(['grep', 'requests'], stdin=pip_list.stdout, stdout=subprocess.PIPE)
+pip_list.stdout.close()
+if grep_requests.communicate()[0].decode() == '':
+  subprocess.run(['pip', 'install', 'requests'])
+
+print('All green.')
