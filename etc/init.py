737ef82026260d6c0188e4097d79aaacaf86e5e7
tick-taku <tick.taku.77@gmail.com>
2021-05-02 16:29:01 +0900

    Rewrite python code.

--- etc/init.py
+++ etc/init.py
@@ -1,15 +1,23 @@
 import subprocess
+import shutil
 
 integration_token = input('integration token: ')
 
-subprocess.run(['cp', 'secrets/secrets_sample.py', './secrets.py'])
-subprocess.run(['sed', '-i', '-e', f's/INTEGRATION_TOKEN =/INTEGRATION_TOKEN = "{integration_token}"/', 'secrets.py'])
-subprocess.run(['rm', '-rf', 'secrets.py-e'])
+shutil.copy('secrets/secrets_sample.py', 'secrets.py')
+
+with open('secrets.py') as f:
+  origin = f.read()
+  replace = origin.replace('INTEGRATION_TOKEN =', f'INTEGRATION_TOKEN = "{integration_token}"')
+
+with open('secrets.py', mode='w') as f:
+  f.write(replace)
+
 
 pip_list = subprocess.Popen(['pip', 'list'], stdout=subprocess.PIPE)
 grep_requests = subprocess.Popen(['grep', 'requests'], stdin=pip_list.stdout, stdout=subprocess.PIPE)
 pip_list.stdout.close()
 if grep_requests.communicate()[0].decode() == '':
   subprocess.run(['pip', 'install', 'requests'])
+
 
 print('All green.')
